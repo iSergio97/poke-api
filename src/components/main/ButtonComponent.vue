13b95594d72aeec00210a008f5b056bac77ac48e
Sergio Garrido Domínguez <sergiotb15@gmail.com>
2025-03-29 14:27:04 +0100

    Nuevo componente para cargar más pokemons en la tabla

---
+++ src/components/main/ButtonComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <button
+    type="button"
+    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    :disabled="loading"
+  >
+    <svg
+      class="w-4 h-4"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 14 10"
+    >
+      <path
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M1 5h12m0 0L9 1m4 4L9 9"
+      />
+    </svg>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue';
+
+interface Props {
+  loading: boolean;
+}
+
+const props = defineProps<Props>();
+
+const { loading } = toRefs(props);
+</script>
+
+<style scoped></style>
