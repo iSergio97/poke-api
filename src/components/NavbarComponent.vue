99e4285a9521f5ac1f303326c241cd05dcd80a31
Sergio Garrido Domínguez <sergiotb15@gmail.com>
2025-07-13 14:25:36 +0200

    SVG Buscador

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -32,21 +32,7 @@
         </button>
         <div class="relative hidden md:block">
           <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg
-              class="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
+            <PokemonSVGComponent />
             <span class="sr-only">Search icon</span>
           </div>
           <input
@@ -103,16 +89,29 @@
               />
             </svg>
           </div>
-          <input
-            type="text"
-            id="search-navbar"
-            class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-            placeholder="Buscar Pokemon"
-          />
+          <div class="flex">
+            <span
+              class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
+            >
+            <PokemonSVGComponent />
+            </span>
+            <input
+              type="text"
+              v-model="search"
+              id="search-navbar"
+              class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Buscar Pokemon"
+            />
+          </div>
         </div>
       </div>
     </div>
   </nav>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import PokemonSVGComponent from './PokemonSVGComponent.vue';
+
+const search = ref('');
+</script>
